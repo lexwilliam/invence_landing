c1a15cff23daa7c8ee6dd128436b8802049bc10d
lexwilliam <william.gumdamx@gmail.com>
2024-10-02 14:15:34 +0700

    test 6

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -17,41 +17,41 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
     try {
-        adminAuth.verifyIdToken(idToken);
-        // const body = (await req.json()) as Transaction;
-        // let summary;
-        // const docId = createTransactionSummaryId(body);
-        // const doc = await adminFirestore
-        //     .collection(firestoreConfig.collection.transaction_summary)
-        //     .doc(docId)
-        //     .get();
-        // if (doc.exists) {
-        //     const oldSummary = doc.data() as TransactionSummary;
-        //     summary = {
-        //         uuid: oldSummary.uuid,
-        //         branch_uuid: oldSummary.branch_uuid,
-        //         date: oldSummary.date,
-        //         total: oldSummary.total,
-        //         profit: oldSummary.profit,
-        //     };
-        // } else {
-        //     summary = {
-        //         uuid: docId,
-        //         branch_uuid: body.branch_uuid,
-        //         date: body.created_at,
-        //         total: 0,
-        //         profit: 0,
-        //     };
-        // }
-        // adminFirestore
-        //     .collection(firestoreConfig.collection.transaction_summary)
-        //     .doc(docId)
-        //     .set(summary);
-        // adminFirestore
-        //     .collection(firestoreConfig.collection.transaction)
-        //     .doc(docId)
-        //     .set(body);
-        return NextResponse.json({ message: 'ok' }, { status: 200 });
+        // adminAuth.verifyIdToken(idToken);
+        const body = (await req.json()) as Transaction;
+        let summary;
+        const docId = createTransactionSummaryId(body);
+        const doc = await adminFirestore
+            .collection(firestoreConfig.collection.transaction_summary)
+            .doc(docId)
+            .get();
+        if (doc.exists) {
+            const oldSummary = doc.data() as TransactionSummary;
+            summary = {
+                uuid: oldSummary.uuid,
+                branch_uuid: oldSummary.branch_uuid,
+                date: oldSummary.date,
+                total: oldSummary.total,
+                profit: oldSummary.profit,
+            };
+        } else {
+            summary = {
+                uuid: docId,
+                branch_uuid: body.branch_uuid,
+                date: body.created_at,
+                total: 0,
+                profit: 0,
+            };
+        }
+        adminFirestore
+            .collection(firestoreConfig.collection.transaction_summary)
+            .doc(docId)
+            .set(summary);
+        adminFirestore
+            .collection(firestoreConfig.collection.transaction)
+            .doc(docId)
+            .set(body);
+        return NextResponse.json({ message: 'Success' }, { status: 200 });
     } catch (error) {
         return NextResponse.json('Internal Server Error', { status: 500 });
     }
