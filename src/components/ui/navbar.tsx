283c5ea6331ac9a3d59ca91f477fc1cd8d80e408
lexwilliam <william.gumdamx@gmail.com>
2024-10-04 15:07:59 +0700

    populate navbar content and remove icon deps

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,10 +1,18 @@
 import { cn } from '@/lib/utils';
 import { useState } from 'react';
-import { MenuItem, HoveredLink, ProductItem, Menu } from './navbar-menu';
+import { MenuItem, HoveredLink, Menu } from './navbar-menu';
 import { assets } from '@/app/assets';
-import Image from 'next/image';
-import { span } from 'framer-motion/client';
-import { DownloadIcon, HamburgerMenuIcon } from '@radix-ui/react-icons';
+import {
+    ArchiveIcon,
+    ChartNoAxesCombinedIcon,
+    CircleUserIcon,
+    MenuIcon,
+    NewspaperIcon,
+    PlayIcon,
+    ShoppingCartIcon,
+    StoreIcon,
+} from 'lucide-react';
+import Link from 'next/link';
 
 export function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
@@ -15,7 +23,7 @@ export function Navbar({ className }: { className?: string }) {
                 className,
             )}>
             <Menu setActive={setActive}>
-                <div className="flex flex-row gap-4 items-center w-16">
+                <div className="flex flex-row gap-4 items-center w-20">
                     <img
                         className="w-10 h-10 rounded-lg"
                         src={assets.invenceLogo}
@@ -28,61 +36,122 @@ export function Navbar({ className }: { className?: string }) {
                         <MenuItem
                             setActive={setActive}
                             active={active}
-                            item="Services">
-                            <div className="flex flex-col space-y-4 text-sm">
-                                <HoveredLink href="/web-dev">
-                                    Web Development
+                            item="Features">
+                            <div className="flex flex-col gap-4 text-sm">
+                                <HoveredLink href="/features/inventory">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <ArchiveIcon />
+                                        <span>Inventory Management</span>
+                                    </div>
                                 </HoveredLink>
-                                <HoveredLink href="/interface-design">
-                                    Interface Design
+                                <HoveredLink href="/features/ordering">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <ShoppingCartIcon />
+                                        <span>Ordering & Sales</span>
+                                    </div>
                                 </HoveredLink>
-                                <HoveredLink href="/seo">
-                                    Search Engine Optimization
-                                </HoveredLink>
-                                <HoveredLink href="/branding">
-                                    Branding
+                                <HoveredLink
+                                    href="/features/analytics"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <ChartNoAxesCombinedIcon />
+                                        <span>Analytics</span>
+                                    </div>
                                 </HoveredLink>
                             </div>
                         </MenuItem>
                         <MenuItem
                             setActive={setActive}
                             active={active}
-                            item="Products">
-                            <div className="  text-sm grid grid-cols-2 gap-10 p-4"></div>
+                            item="Solution">
+                            <div className="flex flex-col space-y-4 text-sm">
+                                <HoveredLink
+                                    href="/solution/online-shop"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <ShoppingCartIcon />
+                                        <span>Online Shop</span>
+                                    </div>
+                                </HoveredLink>
+                                <HoveredLink
+                                    href="/solution/"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <StoreIcon />
+                                        <span>Retail</span>
+                                    </div>
+                                </HoveredLink>
+                            </div>
                         </MenuItem>
                         <MenuItem
                             setActive={setActive}
                             active={active}
-                            item="Pricing">
+                            item="Resources">
                             <div className="flex flex-col space-y-4 text-sm">
-                                <HoveredLink href="/hobby">Hobby</HoveredLink>
-                                <HoveredLink href="/individual">
-                                    Individual
+                                <HoveredLink
+                                    href="/blog"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <NewspaperIcon />
+                                        <span>Blog</span>
+                                    </div>
                                 </HoveredLink>
-                                <HoveredLink href="/team">Team</HoveredLink>
-                                <HoveredLink href="/enterprise">
-                                    Enterprise
+                                <HoveredLink
+                                    href="/guides"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <PlayIcon />
+                                        <span>Guides</span>
+                                    </div>
+                                </HoveredLink>
+                                <HoveredLink
+                                    href="/about-us"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <CircleUserIcon />
+                                        <span>About us</span>
+                                    </div>
                                 </HoveredLink>
                             </div>
                         </MenuItem>
+                        <Link href={'/pricing'}>Pricing</Link>
                     </div>
                 </div>
                 <div className="hidden sm:block my-auto">
-                    <div className="flex items-center w-16">
+                    <div className="flex items-center w-20">
                         <MenuItem
                             setActive={setActive}
                             active={active}
-                            item="Install"
-                            itemChildren={
-                                <div className="flex flex-row gap-4 items-center">
-                                    <span>Install</span>
-                                    <DownloadIcon />
-                                </div>
-                            }></MenuItem>
+                            item="Install now">
+                            <div className="flex flex-col space-y-4 text-sm">
+                                <HoveredLink
+                                    href="/"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <img
+                                            src={assets.androidLogo}
+                                            className="w-6 h-6"
+                                        />
+                                        <span>Android</span>
+                                    </div>
+                                </HoveredLink>
+                                <HoveredLink
+                                    href="/"
+                                    className="flex flex-row items-center">
+                                    <div className="flex flex-row items-center gap-4">
+                                        <img
+                                            src={assets.iosLogo}
+                                            className="w-6 h-6 object-contain"
+                                        />
+                                        <span>IOS (Coming soon)</span>
+                                    </div>
+                                </HoveredLink>
+                            </div>
+                        </MenuItem>
                     </div>
                 </div>
                 <div className="flex items-center sm:hidden">
-                    <HamburgerMenuIcon className="block sm:hidden w-6 h-6" />
+                    <MenuIcon className="block sm:hidden w-6 h-6" />
                 </div>
             </Menu>
         </div>
